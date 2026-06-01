544177a3ecfe32090cd72fa96540df955f0052af
massimilianomiceli <miceli.massimiliano@geoconsultsrl.it>
2026-06-01 15:57:24 +0200

    creata classe Genre ogni movie ha un genre

--- index.php
+++ index.php
@@ -1,4 +1,18 @@
 <?php
+
+class Genre
+{
+    public $name;
+    public $description;
+
+    public function __construct($_name, $_description)
+    {
+        $this->name = $_name;
+        $this->description = $_description;
+    }
+}
+
+
 class Movie
 {
     public $title;
@@ -6,12 +20,13 @@ class Movie
     public $director;
     public $duration;
 
-    public function __construct($_title, $_year, $_director, $_duration)
+    public function __construct($_title, $_year, $_director, $_duration, Genre $_genre)
     {
         $this->title = $_title;
         $this->year = $_year;
         $this->director = $_director;
         $this->duration = $_duration;
+        $this->genre = $_genre;
     }
 
     public function getMovie()
@@ -19,9 +34,13 @@ class Movie
         return $this->title . " was recorded in " . $this->year . " by " . $this->director . " and has a duration of " . $this->duration . " minutes.";
     }
 }
+$InceptionGenre = new Genre("Sci-Fi", "A genre that combines scientific or futuristic elements with a tense story full of mystery, suspense, and plot twists");
+$Inception = new Movie("Inception", 2010, "Christopher Nolan", 148, $InceptionGenre);
+$InterstellarGenre = new Genre("Science Fiction", "A genre based on scientific, technological, futuristic, or space-related elements, often exploring imaginary worlds, advanced technology, or alternative realities.");
+$Interstellar = new Movie("Interstellar", 2014, "Christopher Nolan", 169, $InterstellarGenre);
 
-$Inception = new Movie("Inception", 2010, "Christopher Nolan", 148);
-$Interstellar = new Movie("Interstellar", 2014, "Christopher Nolan", 169);
+var_dump($Inception);
+var_dump($Interstellar);
 
 echo $Inception -> getMovie();
 echo "<br>";

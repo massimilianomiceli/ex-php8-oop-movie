39131922d707e3a4f1a1d3236a1fc95f74dcec8b
massimilianomiceli <miceli.massimiliano@geoconsultsrl.it>
2026-06-01 15:27:26 +0200

    creata classe Movie con costrutture e metodo

--- index.php
+++ index.php
@@ -1,3 +1,32 @@
+<?php
+class Movie
+{
+    public $title;
+    public $year;
+    public $director;
+    public $duration;
+
+    public function __construct($_title, $_year, $_director, $_duration)
+    {
+        $this->title = $_title;
+        $this->year = $_year;
+        $this->director = $_director;
+        $this->duration = $_duration;
+    }
+
+    public function getMovie()
+    {
+        return $this->title . " was recorded in " . $this->year . " by " . $this->director . " and has a duration of " . $this->duration . " minutes.";
+    }
+}
+
+$Inception = new Movie("Inception", 2010, "Christopher Nolan", 148);
+$Interstellar = new Movie("Interstellar", 2014, "Christopher Nolan", 169);
+
+echo $Inception -> getMovie();
+echo "<br>";
+echo $Interstellar -> getMovie();
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>

177837766e5556e617d3f3dda60379db36f7b607
massimilianomiceli <miceli.massimiliano@geoconsultsrl.it>
2026-06-01 16:15:01 +0200

    modificata classe movie in modo tale che accetti piu genre

--- index.php
+++ index.php
@@ -19,25 +19,33 @@ class Movie
     public $year;
     public $director;
     public $duration;
+    public $genres;
 
-    public function __construct($_title, $_year, $_director, $_duration, Genre $_genre)
+    public function __construct($_title, $_year, $_director, $_duration, $_genres)
     {
         $this->title = $_title;
         $this->year = $_year;
         $this->director = $_director;
         $this->duration = $_duration;
-        $this->genre = $_genre;
+        $this->genres = $_genres;
     }
 
     public function getMovie()
     {
-        return $this->title . " was recorded in " . $this->year . " by " . $this->director . " and has a duration of " . $this->duration . " minutes.";
+        $genresList = [];
+
+        foreach ($this->genres as $genre) {
+            $genresList[] = $genre->name;
+        }
+
+        //implode restituisce una stringa contenente tutti gli elementi di un array nel medesimo ordine, inserendo il parametro glue tra un elemento e l'altro.
+        return $this->title . " was released in " . $this->year . ", directed by " . $this->director . ", and has a duration of " . $this->duration . " minutes. " . "<br>" . "Genres: " . implode(", ", $genresList);
     }
 }
-$InceptionGenre = new Genre("Sci-Fi", "A genre that combines scientific or futuristic elements with a tense story full of mystery, suspense, and plot twists");
-$Inception = new Movie("Inception", 2010, "Christopher Nolan", 148, $InceptionGenre);
-$InterstellarGenre = new Genre("Science Fiction", "A genre based on scientific, technological, futuristic, or space-related elements, often exploring imaginary worlds, advanced technology, or alternative realities.");
-$Interstellar = new Movie("Interstellar", 2014, "Christopher Nolan", 169, $InterstellarGenre);
+$GenreSciFi = new Genre("Sci-Fi", "A genre that combines scientific or futuristic elements with a tense story full of mystery, suspense, and plot twists");
+$GenreScienceFiction = new Genre("Science Fiction", "A genre based on scientific, technological, futuristic, or space-related elements, often exploring imaginary worlds, advanced technology, or alternative realities.");
+$Inception = new Movie("Inception", 2010, "Christopher Nolan", 148, [$GenreSciFi, $GenreScienceFiction]);
+$Interstellar = new Movie("Interstellar", 2014, "Christopher Nolan", 169, [$GenreSciFi]);
 
 var_dump($Inception);
 var_dump($Interstellar);
